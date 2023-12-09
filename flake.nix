1b0df3f4ce1a50ce97248bb3df1a58a93391e63b
uku <uku3lig@gmail.com>
2023-12-09 22:56:58 +0100

    nix: fmt & add nil

--- flake.nix
+++ flake.nix
@@ -8,18 +8,25 @@
     };
   };
 
-  outputs = {flake-parts, ...} @ inputs: flake-parts.lib.mkFlake {inherit inputs;} {
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
 
-    perSystem = {lib, pkgs, ...}: {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs
-          nodePackages.pnpm
-        ];
+      perSystem = {
+        lib,
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.pnpm
+          ];
+
+          packages = [pkgs.nil];
+        };
+
+        formatter = pkgs.alejandra;
       };
-
-      formatter = pkgs.alejandra;
     };
-  };
 }
